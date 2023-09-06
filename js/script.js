259718c38851a0ac61f93364a3f80d77b127b850
Anya-Ostapchik <anna.ostapchik@gmail.com>
2023-09-06 03:08:24 +0300

    JS Vertical Movement

--- js/script.js
+++ js/script.js
@@ -42,6 +42,69 @@ btnLang.addEventListener('click', function(){
 });
 
 // анимация с движением иконок
+const imgBlock = document.querySelector('.imgs');
 const imgOne = document.querySelector('.img__item_1');
 const imgTwo = document.querySelector('.img__item_2');
 const imgThree = document.querySelector('.img__item_3');
+let speedOne = 1;
+let speedTwo = 1;
+let speedThree = 1;
+
+function movementElems() {
+    let imgOneTop = parseInt(getComputedStyle(imgOne).top);
+    let imgTwoTop = parseInt(getComputedStyle(imgTwo).top);
+    let imgThreeTop = parseInt(getComputedStyle(imgThree).top);
+
+    let imgOnePosY = imgOne.getBoundingClientRect().y;
+    let imgTwoPosY = imgTwo.getBoundingClientRect().y;
+    let imgThreePosY = imgThree.getBoundingClientRect().y;
+
+    const imgOneHeight = parseInt(imgOne.clientHeight);
+    const imgTwoHeight = parseInt(imgTwo.clientHeight);
+    const imgThreeHeight = parseInt(imgThree.clientHeight);
+
+    const imgBlockPosY = imgBlock.getBoundingClientRect().y;
+    const imgBlockHeight = parseInt(imgBlock.clientHeight);
+
+    const topOne = imgBlockPosY + imgBlockHeight - imgOneHeight;
+    const bottomOne = imgBlockPosY + imgOneHeight;
+    const topTwo = imgBlockPosY + imgBlockHeight - imgTwoHeight;
+    const bottomTwo = imgBlockPosY + imgTwoHeight;
+    const topThree = imgBlockPosY + imgBlockHeight - imgThreeHeight;
+    const bottomThree = imgBlockPosY + imgThreeHeight;
+
+    if(window.innerWidth > 916){
+        imgOne.style.top = `${imgOneTop + speedOne}px`;
+
+        if(imgOnePosY < topOne){
+            speedOne = 1;
+        }
+        if(imgOnePosY + imgOneHeight > bottomOne){
+            speedOne = -1;
+        }
+
+        imgTwo.style.top = `${imgTwoTop + speedTwo}px`;
+
+        if(imgTwoPosY < topTwo){
+            speedTwo = 1;
+        }
+        if(imgTwoPosY + imgTwoHeight > bottomTwo){
+            speedTwo = -1;
+        }
+
+        imgThree.style.top = `${imgThreeTop + speedThree}px`;
+
+        if(imgThreePosY < topThree){
+            speedThree = 1;
+        }
+        if(imgThreePosY + imgThreeHeight > bottomThree){
+            speedThree = -1;
+        }
+    } 
+    // else{
+    //     imgOne.style.top = `${imgOneTop + speedOne}px`;
+    // }
+    requestAnimationFrame(movementElems);
+}
+
+requestAnimationFrame(movementElems);
